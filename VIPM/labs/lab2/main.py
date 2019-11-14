8a52a6232e1bda9c0fe3402f920b249a348581c3
gianscarpe <g.scarpellini1@campus.unimib.it>
2019-11-14 11:53:11 +0100

    AI, VIPM

--- VIPM/labs/lab2/main.py
+++ VIPM/labs/lab2/main.py
@@ -45,18 +45,18 @@ def get_default_mask(img1_shape, w_size_blend):
     mask = np.repeat(blend_filter[:, :, np.newaxis],
                      3,
                      axis=2)
-    return mask
+    return mask.astype('float32')
 
 
 def gaussian_pyramid_level(previous_level_image):
     previous_level_dim = (previous_level_image.shape[1] // 2,
                           previous_level_image.shape[0] // 2)
     filtered_img = cv2.GaussianBlur(previous_level_image,
-                                    (GAUSSIAN_FILTER_SIZE, GAUSSIAN_FILTER_SIZE), cv2.BORDER_CONSTANT)
+                                    (GAUSSIAN_FILTER_SIZE, GAUSSIAN_FILTER_SIZE), cv2.BORDER_TRANSPARENT)
 
     current_level_image = cv2.resize(filtered_img, previous_level_dim)
 
-    return current_level_image
+    return current_level_image.astype('float32')
 
 
 def laplacian_pyramid_level(previous_level_image, current_level_image):
@@ -65,7 +65,7 @@ def laplacian_pyramid_level(previous_level_image, current_level_image):
     current_level_image = cv2.resize(current_level_image, previous_level_dim)
 
     result = previous_level_image - current_level_image
-    return result
+    return result.astype('float32')
 
 
 def build_pyramids(img):
@@ -102,13 +102,19 @@ def collapse_pyramid(pyramid):
 
         result = cv2.resize(result, next_level_dim) + next_level
 
-    return result.astype('float32')
+    return result
 
 
 def show_pyramid(pyramid):
     for i in range(len(pyramid)):
-        cv2.imshow(f'Level {i}', pyramid[i])
-        cv2.waitKey()
+
+        toshow = pyramid[i]
+        if toshow.shape[-1] == 3:
+            toshow = toshow.astype('float32')
+            toshow = cv2.cvtColor(toshow, cv2.COLOR_BGR2RGB)
+
+        plt.figure()
+        plt.imshow(toshow)
 
 
 if __name__ == '__main__':
@@ -125,6 +131,7 @@ if __name__ == '__main__':
     right_laplacian_pyramid, right_gaussian_pyramid = build_pyramids(im2)
     _, mask_pyramid = build_pyramids(mask)
 
+    show_pyramid(mask_pyramid)
     combined = combine_pyramids(left_laplacian_pyramid, right_laplacian_pyramid, mask_pyramid)
 
     result = collapse_pyramid(combined)
